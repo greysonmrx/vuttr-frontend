2804ef3e9a0afdea212bd7af5b60510738328a5e
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-09-15 13:27:53 -0300

    test

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -68,7 +68,7 @@ const Login: React.FC = () => {
 
   return (
     <Container>
-      <h1>VUTTR</h1>
+      <h1>VUTTR - Frontend</h1>
       <Form ref={formRef} onSubmit={handleSubmit}>
         <Input
           name="email"
